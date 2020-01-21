4d3bcd228f1171df6f72d150ff649fb9efdc6f5a
lab314-10@labeng.local <lab314-10@labeng.local>
2020-01-21 15:51:44 +0700

    finished

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,42 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream>
+using namespace std;
+
+int main(){
+    string status;
+    int age;
+    double money,height;
+    cout<<"Enter age: ";
+    cin>>age;
+
+    if(age<=20){
+        cout<<"Enter Height: ";
+        cin>>height;
+            if(height<160)
+                status="UNFRIEND";
+            else if(height<175)
+                status="FRIEND";
+            else{
+                cout<<"Enter Money: ";
+                cin>>money;
+                    if(money>690000000){
+                        status="MARRIED";
+                    }else{
+                        status="ONE-NIGHT-STAND";
+                    }
+            }
+    }else{
+        if(age<30){
+            
+            cout<<"Enter Money: ";
+            cin>>money;
+            if(money>100000000){
+                status="BEST FRIEND";
+            }else{
+                status="UNFRIEND";
+            }
+        }else{
+            status="UNFRIEND";
+        }
+    }
+    cout<<status;
+}
